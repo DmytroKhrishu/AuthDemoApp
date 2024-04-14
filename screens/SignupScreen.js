52377b8f6d85ffb09656dfaed8751159c19afca5
DimaKhrishu <dima.khrishu@gmail.com>
2024-04-14 13:33:28 +0300

    enabled signing up via Firebase

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,7 +1,22 @@
+import { useState } from 'react';
 import AuthContent from '../components/Auth/AuthContent';
+import { createUser } from '../util/auth';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
 
 function SignupScreen() {
-  return <AuthContent />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false)
+
+  async function signupHandler({ email, password }) {
+    setIsAuthenticating(true)
+    const response = await createUser(email, password);
+    setIsAuthenticating(false);
+  }
+
+  if(isAuthenticating) {
+    return <LoadingOverlay />
+  }
+
+  return <AuthContent onAuthenticate={signupHandler} />;
 }
 
 export default SignupScreen;
