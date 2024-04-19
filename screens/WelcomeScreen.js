48e06ce5e9965ef1bcfb0094d9c80e50eb75d6f0
DimaKhrishu <dima.khrishu@gmail.com>
2024-04-19 15:52:14 +0300

    utilized AsyncStorage to store the token on device(does not log out if app closed)

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,29 @@
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AuthContext } from '../store/auth-context';
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState('');
+
+  const authCtx = useContext(AuthContext);
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://authdemonative-53aae-default-rtdb.europe-west1.firebasedatabase.app/message.json?auth=' +
+          authCtx.token
+      )
+      .then((response) => {
+        setFetchedMessage(response.data);
+      });
+  }, [ authCtx.token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
